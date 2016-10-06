9b4d26018bf5fe8ddf2bbc9dca6d733316cc6388
trunglhb <LHBTRUNG60@GMAIL.COM>
2016-10-06 16:01:04 +0700

    First commit

---
+++ Web-war/src/main/java/vn/com/demo/DemoApplication.java
@@ -0,0 +1,44 @@
+
+package vn.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.WebApplicationInitializer;
+
+/**
+ * <b>RentalReportApplication</b>.<br>
+ * RentalReportApplication class container<br>
+ * ------------------------<br>
+ * History:<br>
+ * 2016-06-30 - TrungLHB - Initial<br>
+ *
+ * @author TrungLHB
+ */
+@ComponentScan({ "vn.com.demo" })
+//@ImportResource({ "classpath:mirageContext.xml", "classpath:selectSystemDateTime.xml" })
+@SpringBootApplication
+@EnableCaching
+public class DemoApplication extends SpringBootServletInitializer
+                                                            implements WebApplicationInitializer {
+    /**
+     * main method<br>
+     * ------------------------<br>
+     * History:<br>
+     * 2016-06-30 - TrungLHB - Initial<br>
+     *
+     * @param args arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+}
